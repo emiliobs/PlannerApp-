d3a1c9376ae76ff47babe2270cc4593306044240
Emilio <barrera_emilio@yahoo.es>
2020-03-08 21:56:29 +0000

    AlertMessage shared componet

---
+++ PlannerApp/Models/AlertMessageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Models
+{
+    public enum AlertMessageType
+    {
+        Error,
+        Warning,
+        Success
+    }
+}
