c8e26d814bb8610e21552dd61139486d6733aa47
Emilio <barrera_emilio@yahoo.es>
2020-03-08 22:41:02 +0000

    Create Login Componet

---
+++ PlannerApp.Shared/Models/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlannerApp.Shared.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
